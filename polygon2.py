6f703adc13de5466cf555c774a018f63a6e5411a
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2025-07-23 18:37:44 -0300

    Adding function for segment segment intersection

--- polygon2.py
+++ polygon2.py
@@ -4,14 +4,9 @@ from vector2 import Vector2
 
 class Polygon2(tuple[Vector2, ...]):
 
-	def edges(self) -> Iterator[tuple[Vector2, Vector2]]:
+	def edges(self) -> list[tuple[Vector2, Vector2]]:
 		"""
 		Iterate over the edges of the polygon.
 		Each edge is represented as a tuple of two Vector2 points.
 		"""
-
-		for i in range(len(self) - 1):
-			yield (self[i], self[i + 1])
-		
-		if self:
-			yield (self[-1], self[0])
+		return [(self[i], self[(i + 1) % len(self)]) for i in range(len(self))]
--- problem1.py
+++ problem1.py
@@ -11,6 +11,34 @@ We will consider that:
 from vector2 import Vector2
 from polygon2 import Polygon2
 
+def segment_segment_intersection(start1: Vector2, end1: Vector2, start2: Vector2, end2: Vector2) -> Vector2 | None:
+	"""
+	Returns the intersection point of two line segments if they intersect, otherwise returns None.
+
+	:param Vector2 start1: The start point of the first segment as a Vector2.
+	:param Vector2 end1: The end point of the first segment as a Vector2.
+	:param Vector2 start2: The start point of the second segment as a Vector2.
+	:param Vector2 end2: The end point of the second segment as a Vector2.
+
+	:return: The intersection point as a Vector2 if the segments intersect, otherwise None.	
+	"""
+
+	diff1 = end1 - start1
+	diff2 = end2 - start2
+
+	det = diff1.cross(diff2)
+
+	if abs(det) < 1e-8:
+		return None
+
+	sdiff = start2 - start1
+
+	t = sdiff.cross(diff2) / det
+	u = sdiff.cross(diff1) / det
+
+	if 0 <= t <= 1 and 0 <= u <= 1:
+		return start1 + diff1 * t
+
 class Solution:
 
 	start: Vector2
@@ -20,8 +48,8 @@ class Solution:
 	# Indicates whether edge `j` of polygon `i` is blocked
 	blocked: list[list[bool]]
 
-	# Indicates, cone vectors for each polygon vertex
-	# Each cone is represented by a pair of vectors (start, end)
+	# For each polygon and vertex, we store two vectors that represent the cone of visibility.
+	# They are stored in counter-clockwise order.
 	cones: list[list[tuple[Vector2, Vector2]]]
 
 	def __init__(self, start: Vector2, end: Vector2, polygons: list[Polygon2]) -> None:
@@ -29,6 +57,9 @@ class Solution:
 		self.start = start
 		self.end = end
 		self.polygons = polygons
+
+		self.blocked = [[False] * len(polygon) for polygon in polygons]
+		self.cones = [[(Vector2(), Vector2()) for _ in range(len(polygon))] for polygon in polygons]
 
 	def draw(self) -> None:
 		pass
@@ -39,10 +70,19 @@ class Solution:
 		Returns the point that comes before `point`.
 		"""
 
-	def shortest_path(self) -> list[Polygon2]:
+		if index == -1:
+			return self.start
+
+	def shortest_path(self) -> list[Vector2]:
 		
+		if len(self.polygons) == 0:
+			return [self.start, self.end]
+
+		for v1, v2 in self.polygons[0].edges():
+
+			middle = v1.lerp(v2, 0.5)
+			last = self.query(middle, -1)
+
 
 
 		pass
-
-
