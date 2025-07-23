299e7b225d6e5037cb1a10537086c9b5fb7efaf6
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2025-07-23 17:02:11 -0300

    Laying the foundation for the first problem

---
+++ problem1.py
@@ -0,0 +1,10 @@
+"""
+Implementation of the first variation of the problem. 
+
+We will consider that:
+- The polygons are convex
+- There is no "fence"
+- The polygons are non intersecting
+- The problem is in 2D.
+"""
+
