3f671541df2f7e927bd01bd5efb3395a8f09e076
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2025-08-11 01:18:44 -0300

    updating drawing to include legend for all plots

--- problem1_draw.py
+++ problem1_draw.py
@@ -195,10 +195,13 @@ class Drawing(Solution):
 		self.draw_scene(flat[0], -1)
 
 		# Set title for the whole figure
-		fig.suptitle("Shortest path from Start to End passing touching every polygon", fontsize=16) # type: ignore
+		fig.suptitle("Shortest path from Start to End touching every polygon", fontsize=16) # type: ignore
 
 		flat[0].set_title("Final Path", fontsize=14)
 		flat[0].legend()
+
+		for i in range(1, len(scenes) + 1):
+			flat[i].legend()
 
 		plt.show() # type: ignore
 
@@ -252,6 +255,8 @@ class Drawing(Solution):
 				plot(*zip(vertex, p1), color="red", linewidth=2, linestyle='--')
 				plot(*zip(vertex, p2), color="red", linewidth=2, linestyle='--')
 
+			fill([minx], [miny], alpha=0.45, color="red", label="Cone Region")
+
 		def draw_edges() -> None:
 
 			for i in range(len(polygon)):
@@ -268,6 +273,8 @@ class Drawing(Solution):
 				points = locate_edge(v1, ray1, v2, ray2, bbox)
 
 				fill(*zip(*points), alpha=0.45, color="green")
+			
+			fill([minx], [miny], alpha=0.45, color="green", label="Edge Region")
 
 		bbox = self.get_bbox()
 		minx, miny, maxx, maxy = bbox
@@ -278,6 +285,8 @@ class Drawing(Solution):
 
 		# Fill the background with a cyan color
 		fill([minx, minx, maxx, maxx], [miny, maxy, maxy, miny], color="#6abdbe", alpha=0.7)
+		
+		fill([minx], [miny], color="#6abdbe", alpha=0.7, label="Pass Through Region" * (index != -1))
 
 		if 0 <= index < len(self.polygons):
 			polygon = self.polygons[index]
@@ -285,15 +294,15 @@ class Drawing(Solution):
 			draw_edges()
 
 		for i, polygon in enumerate(self.polygons):
-			fill(*zip(*polygon), alpha=0.8, label=f'Polygon {i + 1}')
+			fill(*zip(*polygon), alpha=0.8, label=f'Polygon {i + 1}' * (index == -1 or index == i))
 			plot(*zip(*polygon, polygon[0]), linewidth=2)
 
 		# Plot the final path
 		plot(*zip(*self.final_path), color="purple")
 
 		# Plot the start and end points
-		plot(*zip(self.start), "o", color="green", label='Start', markersize=4)
-		plot(*zip(self.end), "o", color="red", label='End', markersize=4)
+		plot(*zip(self.start), "o", color="green", label='Start' * (index == -1), markersize=4)
+		plot(*zip(self.end), "o", color="red", label='End' * (index == -1), markersize=4)
 
 		# ax.legend() # type: ignore
 		ax.grid() # type: ignore
