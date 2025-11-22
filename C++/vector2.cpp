d5125bdc513aec88548594ba785922822795e38a
Gabriel Ushijima <gabriel_ushijima@hotmail.com>
2025-11-22 00:01:30 -0300

    Starting implementation of Vector2

---
+++ C++/vector2.cpp
@@ -0,0 +1,9 @@
+
+#include "Vector2.h"
+
+Vector2::Vector2() : x(0), y(0) {}
+Vector2::Vector2(float x, float y) : x(x), y(y) {}
+
+Vector2 Vector2::operator+(const Vector2& other) const {
+	return Vector2(x + other.x, y + other.y);
+}
---
+++ C++/vector2.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+class Vector2 {
+	
+	public:
+	float x, y;
+
+	Vector2();
+	Vector2(float x, float y);
+
+	Vector2 operator+(const Vector2& other) const;
+
+};
